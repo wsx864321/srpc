6a03dea31fc48aebed6b2bb9d08147b3261d28ef
gemaizi <864321211@qq.com>
2022-05-18 22:39:45 +0800

    add random loadblance

---
+++ lb/random.go
@@ -0,0 +1,31 @@
+package lb
+
+import (
+	"crypto/rand"
+	"errors"
+	"math/big"
+
+	"github.com/wsx864321/sweet_rpc/discov"
+)
+
+type Random struct {
+}
+
+func NewRandom() LoadBalanceItf {
+	return &Random{}
+}
+
+func (r *Random) Name() string {
+	return "random"
+}
+
+func (r *Random) Pick(service *discov.Service) (*discov.Endpoint, error) {
+	count := len(service.Endpoints)
+	if count == 0 {
+		return nil, errors.New("endpoint is empty")
+	}
+
+	n, _ := rand.Int(rand.Reader, big.NewInt(int64(count)))
+
+	return service.Endpoints[n.Int64()], nil
+}
