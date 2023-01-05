c707c4fb9ce080342672296732e0774c7839ab9d
maizige <864321211@qq.com>
2023-01-05 23:17:29 +0800

    更改压测脚本

--- benchmark/client/client_test.go
+++ benchmark/client/client_test.go
@@ -4,52 +4,59 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
-	"github.com/wsx864321/srpc/interceptor"
-	"github.com/wsx864321/srpc/interceptor/clientinterceptor"
-	strace "github.com/wsx864321/srpc/trace"
-
 	"github.com/wsx864321/srpc/client"
 	"github.com/wsx864321/srpc/discov/etcd"
+	"github.com/wsx864321/srpc/interceptor"
+	"github.com/wsx864321/srpc/interceptor/clientinterceptor"
 	"github.com/wsx864321/srpc/pool"
 )
 
-func BenchmarkClient(b *testing.B) {
-	strace.StartAgent(strace.WithServiceName("helloworld-client"))
-	defer strace.StopAgent()
+const cliCount = 100
 
+func BenchmarkClient(b *testing.B) {
 	req := &HelloWorldReq{
 		Name: "wsx",
 	}
+	clis := make([]*client.Client, cliCount)
+	for i := 0; i < cliCount; i++ {
+		clis[i] = client.NewClient(
+			client.WithServiceName("helloworld"),
+			client.WithDiscovery(etcd.NewETCDRegister(etcd.WithEndpoints([]string{"127.0.0.1:2371"}))),
+			client.WithPool(pool.NewPool(pool.WithInitialCap(10), pool.WithMaxCap(100))),
+			client.WithReadTimeout(5*time.Second),
+			client.WithWriteTimeout(5*time.Second),
+			client.WithInterceptors([]interceptor.ClientInterceptor{clientinterceptor.ClientTimeoutInterceptor()}...),
+		)
+	}
 
-	cli := client.NewClient(
-		client.WithServiceName("helloworld"),
-		client.WithDiscovery(etcd.NewETCDRegister(etcd.WithEndpoints([]string{"127.0.0.1:2371"}))),
-		client.WithPool(pool.NewPool(pool.WithInitialCap(10), pool.WithMaxCap(100))),
-		client.WithReadTimeout(5*time.Second),
-		client.WithWriteTimeout(5*time.Second),
-		client.WithInterceptors([]interceptor.ClientInterceptor{clientinterceptor.ClientTraceInterceptor(), clientinterceptor.ClientTimeoutInterceptor()}...),
-	)
-	defer cli.Close()
+	defer func() {
+		for _, c := range clis {
+			c.Close()
+		}
+	}()
 
-	loopCount := 100000
-	var count = 0
+	reqCount := 1000000
+	var count int64 = 0
 	now := time.Now()
 	wg := sync.WaitGroup{}
-	wg.Add(loopCount)
-	for i := 0; i < loopCount; i++ {
-		go func() {
+	wg.Add(cliCount)
+	for i := 0; i < cliCount; i++ {
+		go func(cc int) {
 			defer wg.Done()
-			var resp HelloWorldResp
-			ctx, _ := context.WithTimeout(context.TODO(), 4*time.Second)
-			err := cli.Call(ctx, "SayHello", req, &resp)
-			if err != nil {
-				count++
-				fmt.Println(err)
+			for j := 0; j < reqCount/cliCount; j++ {
+				var resp HelloWorldResp
+				ctx, _ := context.WithTimeout(context.TODO(), 4*time.Second)
+				err := clis[cc].Call(ctx, "SayHello", req, &resp)
+				if err != nil {
+					atomic.AddInt64(&count, 1)
+				}
 			}
-		}()
+
+		}(i)
 
 	}
 
